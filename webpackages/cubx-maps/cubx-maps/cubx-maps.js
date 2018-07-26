f22598dfd818297ec2b0d67be0e524994f0177ae
edwingamboa <edwingamboa355@gmail.com>
2018-07-26 07:27:39 -0500

    #2: Add cubx-maps files

--- webpackages/cubx-maps/cubx-maps/cubx-maps.js
+++ webpackages/cubx-maps/cubx-maps/cubx-maps.js
@@ -33,6 +33,29 @@
      * Manipulate an element’s local DOM when the cubbles framework is initialized and ready to work.
      */
     contextReady: function () {
+      /*
+      var mymap = L.map(this.$$('#mapDiv')).setView([51.505, -0.09], 13);
+      L.tileLayer('https://a.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        //attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        accessToken: 'your.mapbox.access.token'
+      }).addTo(mymap);
+      var marker = L.marker([51.5, -0.09]).addTo(mymap);
+      var circle = L.circle([51.508, -0.11], {
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.5,
+        radius: 500
+      }).addTo(mymap);
+      var polygon = L.polygon([
+        [51.509, -0.08],
+        [51.503, -0.06],
+        [51.51, -0.047]
+      ]).addTo(mymap);
+      marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+      circle.bindPopup("I am a circle.");
+      polygon.bindPopup("I am a polygon.");
+      */
     },
 
     /**
